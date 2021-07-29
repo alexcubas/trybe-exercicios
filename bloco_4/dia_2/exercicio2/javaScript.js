e3177a82d12b0f1982794718fcef552dafe06c3b
Alex <alex_cubas1.0@hotmail.com>
2021-07-29 18:49:35 -0300

    exercicio 2 ao 5

--- bloco_4/dia_2/exercicio2/javaScript.js
+++ bloco_4/dia_2/exercicio2/javaScript.js
@@ -1,3 +1,47 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 100, 8, 2, 35, 27,];
 
-console.log(numbers)
+//EXERCICIO 1
+
+// console.log(numbers)
+
+//EXERCICIO 2
+
+// contaToda = 0;
+// for (contador = 0; contador < numbers.length; contador += 1) {
+//   contaToda += numbers[contador]
+// }
+// console.log(contaToda)
+
+//EXERCICIO 3
+
+// let contador = 0;
+// let contadorDivisorio = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   contador += numbers[index]
+//     contadorDivisorio = contador / numbers.length
+// }
+// console.log(contadorDivisorio)
+
+//EXERCICIO 4
+
+// let contador = 0;
+// let contadorDivisorio = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   contador += numbers[index]
+//     contadorDivisorio = contador / numbers.length
+   
+// }
+// if (contadorDivisorio > 20) {
+//   console.log("valor maior que 20");
+// } else {
+//   console.log("valor menor ou igual a 20");
+// }
+
+//EXERCICIO 5
+
+// let maiorN = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > maiorN) {
+//     maiorN =+ numbers[index]
+//   }
+// } console.log(maiorN)
