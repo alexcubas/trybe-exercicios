697c67e548c8c5a4c8b8979e13c8a55368b705c7
Alex <alex_cubas1.0@hotmail.com>
2021-07-30 15:24:11 -0300

    exercicios bonus

---
+++ bloco_4/dia_2/exercicio1/javascript.js
@@ -0,0 +1,24 @@
+// let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
+
+// tasksList.push("fazer execícios da Trybe")
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask)
+
+// let menu = ["Home", "Serviços", "Portifólio", "Links"]
+// let menuServices = menu[1]
+// let indexPortifolio = menu[2]
+// menu.push("Contato")
+
+// console.log(menu)
+
+// let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"]
+
+// for (var lista = 0; lista < groceryList.length; lista++) {
+//   var listaFeita = groceryList[lista]
+// }
+
+// console.log("Precisamos comprar: " + listaFeita)
