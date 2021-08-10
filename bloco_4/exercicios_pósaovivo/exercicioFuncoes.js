f2b94347e8c7f3181848c04f0b5d1ebfed463a09
Alex <alex_cubas1.0@hotmail.com>
2021-08-10 14:57:07 -0300

    exercicios 5.1

---
+++ bloco_4/exercicios_pósaovivo/exercicioFuncoes.js
@@ -0,0 +1,31 @@
+//EXERCICIO 1
+
+// let palavra = "arara"
+
+// function ePalindromo(s) {
+//   let reverse = s.split("").reverse().join("")
+//   if (reverse === s) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(ePalindromo(palavra))
+
+//EXERCICIO 2
+
+// let arrayNumeros = [2, 3, 6, 7, 10, 1];
+// function maiorValor(arraydoido) {
+//   let maiorNumero = arrayNumeros[0]
+//   let maiorIndex = 0;
+//   for(let index = 0; index < arraydoido.length; index += 1) {
+//     if(maiorNumero < arraydoido[index]) {
+//       maiorNumero = arraydoido[index];
+//       maiorIndex = index
+//     }
+//   }
+//   return maiorIndex
+// }
+
+// console.log(maiorValor(arrayNumeros))
---
+++ bloco_4/exercicios_pósaovivo/javascript.js
@@ -0,0 +1,45 @@
+//EXERCICIO 1
+
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// console.log("bem-vinda, " + info.personagem)
+
+//EXERCICIO 2
+
+// info.recorrente = "Sim"
+
+// console.log("recorrencia: " + info.recorrente)
+
+//EXERCICIO 3
+
+// for (let informacao in info) {
+//   console.log(informacao)
+// }
+
+//EXERCICIO 4
+
+// for ( let informacao in info) {
+//   console.log(info[informacao])
+// }
+
+//EXERCICIO 5
+
+// let secondInfo = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O último MacPatinhas',
+//   recorrente: "Sim"
+// };
+
+
+//   for (let perso in info, secondInfo) {
+// if (perso === "recorrente" && info[perso] === "Sim" && secondInfo[perso] === "Sim") {
+//   console.log("Ambos recorrentes");
+//   } else {
+//   console.log(info[perso] + " e " + secondInfo[perso])
+//   }
+// }
