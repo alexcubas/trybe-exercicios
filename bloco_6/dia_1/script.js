a33cad0696797c7772da1ff0e4458034fed5dda5
Alex <alex_cubas1.0@hotmail.com>
2021-08-19 15:35:10 -0300

    exercicio 1 e 2

---
+++ bloco_6/dia_1/script.js
@@ -0,0 +1,20 @@
+let arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+
+let arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+let estates = document.querySelector("#state-tag")
+
+for (let index = 0; index < arrayTextoEstado.length; index += 1) {
+  let estadosT = arrayTextoEstado[index]
+
+  let textosEstados = document.createElement("option")
+textosEstados.innerText = estadosT
+estates.appendChild(textosEstados)
+textosEstados.value = arraySiglaEstado[index]
+}
+
+let dataDia = document.querySelector("#data-inicio-dia")
+parseInt(dataDia)
+if (dataDia < 0 && dataDia > 31) {
+  alert("escolha um dia valido")
+}
