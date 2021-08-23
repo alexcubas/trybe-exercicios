7b7bdef8f64092ab8638004f25778e66dfd7de0a
Alex <alex_cubas1.0@hotmail.com>
2021-08-23 13:16:10 -0300

    tentando adicionar o resto dos exercicio

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -1,3 +1,6 @@
+import './node_modules/just-validate/dist/js/just-validate.min.js'
+
+
 let arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
 
 let arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
@@ -14,5 +17,6 @@ textosEstados.value = arraySiglaEstado[index]
 }
 
 let date = document.getElementById('date-tag').DatePickerX.init({format: "dd/mm/yyyy"});
+
 
 // window.DatePickerX.setDefaults( Object, options() )
